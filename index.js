6a4439d598d34323fb077c9aaae5f44ee91ec424
Johnny Chen <johnnyc1314@gmail.com>
2022-04-05 16:23:51 -0400

    commit tree 2f2b8107e70b7357c9935ef19ac297446e942dce

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => arr.slice(0,2);
+const returnLastTwoDrivers = (arr) => arr.slice(-2);
+function createFareMultiplier(n){
+    return function (fare){
+            return fare*n
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+//fix selectingDrivers +     selectDifferentDrivers(arrayOfDrivers, function)
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function selectDifferentDrivers(arr, cb){
+    return cb(arr)
+}
+
